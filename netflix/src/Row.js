7e20c814a3d84b10b50b522ec9536e7cc5562f40
Abhinav Gunishetty <abhinav.gunishetty@steinias.com>
2020-11-22 01:35:01 +0000

    Added youtube embed for trailer

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,12 +1,18 @@
 /* eslint-disable array-callback-return */
 import React, {useEffect, useState} from 'react'
 import axios from './axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import './Row.css';
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
 
     const [movies, setMovies] = useState([]);
-
+    const [trailerUrl, setTrailerUrl] = useState("");
     useEffect(()=>{
         async function fetchData(){
            const request = await axios.get(fetchUrl);
@@ -15,16 +21,42 @@ function Row({ title, fetchUrl, isLargeRow }) {
         }
         fetchData();
     },[fetchUrl]);
+
+    const opts = {
+        height:"400",
+        width:"100%",
+        playerVars:{
+            autoplay:1,
+        },
+
+    }
+
+    
+  const handleClick = (movie) => {
+    // console.table(movie?.title)
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.title || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        }).catch((error) => console.log(error));
+    }
+  }
+
     return (
         <div className="row">
-            <h2>{title}</h2>
+            <h2 className="row__title">{title}</h2>
             <div className="row__posters">
+                
                 {movies.map(movie => (
-                    <img key={movie.id} className={`row__poster ${isLargeRow && "row__poster--large" }`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
+                    <img onClick={() => handleClick(movie)} key={movie.id} className={`row__poster ${isLargeRow && "row__poster--large" }`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
 
-export default Row
+export default Row;
